b2b40128ac8e8ecb4c47d3c4a96177815391946f
David Weiland <weiland.david@icloud.com>
2021-08-19 16:01:17 +0200

    Modif if formatDate

--- src/app/format.js
+++ src/app/format.js
@@ -1,11 +1,11 @@
 export const formatDate = (dateStr) => {
-  if (dateStr !== "") {
+  if (dateStr) {
     const date = new Date(dateStr)
     const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
     const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date)
     const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
     const month = mo.charAt(0).toUpperCase() + mo.slice(1)
-    return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,4)}`
+    return `${parseInt(da)} ${month.substr(0, 3)}. ${ye.toString().substr(2, 4)}`
   }
 }
  
